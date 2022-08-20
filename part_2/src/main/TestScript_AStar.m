a35b50dc30e87eedeeace886e283292df16d8a62
clever0ne <ilya-pankoff1999@yandex.ru>
2022-08-20 19:44:31 +0300

    Добавляю лабораторную работу №6.

---
+++ part_2/src/main/TestScript_AStar.m
@@ -0,0 +1,12 @@
+%% определение карты
+clc; clear; close all;
+map = false(10);
+
+% добавление препятствия
+map (1:5, 6) = true;
+
+start_coords = [6, 2];
+dest_coords  = [8, 9];
+
+%%
+[route, numExpanded] = AStarGrid(map, start_coords, dest_coords, true);
